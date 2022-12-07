86448ee30230753814ab5bdf7c42e5d1f098389a
ianMuchesia <ianmuchesia@yahoo.com>
2022-12-07 00:02:39 -0800

    CHORE routing via next_js

---
+++ components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import headerStyles from '../styles/Header.module.css'
+
+const Header = () => {
+  return (
+    <div>
+        <h1 className={headerStyles.title}>
+            <span>WebDev</span>News
+        </h1>
+        <p className={headerStyles.description}>Keep upto date with latest webdev news </p>
+    </div>
+  )
+}
+
+export default Header
